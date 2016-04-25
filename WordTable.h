9643969513d5c0ab834dd7d5689174bba311d230
JustinWillson <juwi8104@colorado.edu>
2016-04-25 16:34:42 -0600

    Added header file for hash table

---
+++ WordTable.h
@@ -0,0 +1,32 @@
+#ifndef WORDTABLE_H
+#define WORDTABLE_H
+#include <string>
+#include <iostream>
+#include <fstream>
+
+struct word{
+    std::string french;
+    std::string english;
+    std::string type;
+    word* next = NULL;
+};
+
+class WordTable{
+    private:
+        word* WordTable[300];
+    public:
+        WordTable();
+        ~WordTable();
+        void practiceNouns();
+        void practiceVerbs();
+        void practiceAdjectives();
+        int getIndex(std::string wordFrench);
+        void addWord(std::string french, std::string english, std::string type);
+        void addFromFile(std::string filename);
+        std::string englishFromFrench(std::string french);
+        void printTable();
+};
+
+
+
+#endif
