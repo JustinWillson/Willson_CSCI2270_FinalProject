b4abaec93a8d84fc0969d424dd812be8953a2853
JustinWillson <juwi8104@colorado.edu>
2016-04-25 19:58:57 -0600

    Implemented driver class

--- Driver.cpp
+++ Driver.cpp
@@ -1,5 +1,46 @@
 #include <iostream>
+#include "WordTable.h"
+using namespace std;
+void showMenu();
 
-int main(){
-    std::cout << "Hello World" << std::endl;
+int main(int argc, char* argv[]){
+    WordTable* myTable = new WordTable;
+    myTable->addFromFile(argv[1]);
+    string userInput = "0";
+    
+    while( userInput != "6" ){
+        showMenu();
+        cin >> userInput;
+        if (userInput == "1"){
+            myTable->printTable();
+        } else if (userInput == "2") {
+            string word;
+            cout << "Please input a word in french" << endl;
+            cin.ignore();
+            getline(cin, word);
+            
+            myTable->englishFromFrench(word);
+        } else if (userInput == "3") {
+            myTable->practiceNouns();
+        } else if (userInput == "4") {
+            myTable->practiceNouns();
+        } else if (userInput == "5") {
+            myTable->practiceNouns();
+        } else if (userInput == "6"){
+            cout << "Goodbye!" << endl;
+        } else {
+            cout << "INVALID INPUT" << endl;
+        }
+    }
+    
+}
+
+void showMenu(){
+    cout << "=====Main Menu=====" << endl;
+    cout << "1. Print Table" << endl;
+    cout << "2. Lookup word" << endl;
+    cout << "3. Practice Nouns" << endl;
+    cout << "4. Practice Verbs" << endl;
+    cout << "5. Practice Adjectives" << endl;
+    cout << "6. Quit" << endl;
 }
